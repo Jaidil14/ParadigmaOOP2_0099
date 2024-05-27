8d685cc720f653c09316bf007516fb9a6c4c5cc4
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-27 15:11:47 +0700

    Menambahkan class turunan ketiga

--- multiWaris.cpp
+++ multiWaris.cpp
@@ -23,3 +23,14 @@ public:
         cout << "pelajar dibuat\n" << endl;
      }
 };
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+    pekerja(pUmur),
+    pelajar(pUmur),
+    orang(pUmur)
+{
+    cout << "Budi dibuat\n" << endl;
+}
+};
